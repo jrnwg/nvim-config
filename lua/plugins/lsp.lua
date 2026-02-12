ca7b1526b3c2aac059e3f3e853b3d044ca1fb78d
jrnwg <189479406+jrnwg@users.noreply.github.com>
2026-02-13 00:46:13 +0100

    formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,15 +4,10 @@ return {
     init = function()
       -- Configure LSP floating windows with rounded borders
       -- Use init instead of config to run before plugin loads
-      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-        vim.lsp.handlers.hover,
-        { border = "rounded" }
-      )
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 
-      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-        vim.lsp.handlers.signature_help,
-        { border = "rounded" }
-      )
+      vim.lsp.handlers["textDocument/signatureHelp"] =
+        vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
     end,
     config = function()
       -- Enable lua_ls
